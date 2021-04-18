1457d64d7257af5f4ed8ba7b994af060829899ec
bogdau <74491913+bogdau@users.noreply.github.com>
2021-04-18 22:20:32 +0300

    Update task_01.cpp

--- prata_cpp/Chapter_2/task_01.cpp
+++ prata_cpp/Chapter_2/task_01.cpp
@@ -1,1 +1,9 @@
+/*Write a C++ program that displays your name and address (or if you value
+ * yourprivacy,a fictitious name and address).*/
+#include <iostream>
 
+using namespace std;
+int main() {
+  cout << "Bogdan Kharitonov" << endl;
+  cout << "25 Khreschatyk str, Kyiv, Ukraine" << endl;
+}
